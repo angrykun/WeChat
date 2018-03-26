93bf2af3e7ced7434998bc48b970287545b30739
angrykun <2545662859@qq.com>
2018-03-26 16:29:16 +0800

    添加sessionHelper

---
+++ WeChat.Solution/WeChat.Common/SessionHelper.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace WeChat.Common
+{
+    /// <summary>
+    /// Session 操作类
+    /// 1、GetSession(string name)根据session名获取session对象
+    /// 2、SetSession(string name, object val)设置session
+    /// </summary>
+    public class SessionHelper
+    {
+        #region 添加Session，调动有效期为20分钟
+        /// <summary>
+        /// 添加Session，调动有效期为20分钟
+        /// </summary>
+        /// <param name="strSessionName">Session对象名称</param>
+        /// <param name="strValue">Session值</param>
+        public static void Add(string strSessionName, string strValue)
+        {
+            HttpContext.Current.Session[strSessionName] = strValue;
+            HttpContext.Current.Session.Timeout = 20;
+        } 
+        #endregion
+
+        #region 添加Session
+        /// <summary>
+        /// 添加Session
+        /// </summary>
+        /// <param name="strSessionName">Session对象名称</param>
+        /// <param name="strValue">Session值</param>
+        /// <param name="iExpires">调动有效期（分钟）</param>
+        public static void Add(string strSessionName, string strValue, int iExpires)
+        {
+            HttpContext.Current.Session[strSessionName] = strValue;
+            HttpContext.Current.Session.Timeout = iExpires;
+        } 
+        #endregion
+
+        #region 添加Session
+        /// <summary>
+        /// 添加Session
+        /// </summary>
+        /// <param name="strSessionName">Session对象名称</param>
+        /// <param name="strValues">Session值数组</param>
+        /// <param name="iExpires">调动有效期（分钟）</param>
+        public static void Adds(string strSessionName, string[] strValues, int iExpires)
+        {
+            HttpContext.Current.Session[strSessionName] = strValues;
+            HttpContext.Current.Session.Timeout = iExpires;
+        } 
+        #endregion
+
+        #region 读取某个Session对象值
+        /// <summary>
+        /// 读取某个Session对象值
+        /// </summary>
+        /// <param name="strSessionName">Session对象名称</param>
+        /// <returns>Session对象值</returns>
+        public static string Get(string strSessionName)
+        {
+            if (HttpContext.Current.Session[strSessionName] == null)
+            {
+                return null;
+            }
+            else
+            {
+                return HttpContext.Current.Session[strSessionName].ToString();
+            }
+        } 
+        #endregion
+
+
+        #region 读取某个Session对象值
+        /// <summary>
+        ///  读取某个Session对象值
+        /// </summary>
+        /// <typeparam name="T">类型</typeparam>
+        /// <param name="strSessionName">Session对象名称</param>
+        /// <returns>Session对象值</returns>
+        public static T Get<T>(string strSessionName)
+        {
+
+            if (HttpContext.Current.Session[strSessionName] == null)
+            {
+                return default(T);
+            }
+            else
+            {
+                try
+                {
+                    return (T)Convert.ChangeType(HttpContext.Current.Session[strSessionName], typeof(T));
+                }
+                catch { return default(T); }
+
+            }
+        } 
+        #endregion
+
+        #region 读取某个Session对象值数组
+        /// <summary>
+        /// 读取某个Session对象值数组
+        /// </summary>
+        /// <param name="strSessionName">Session对象名称</param>
+        /// <returns>Session对象值数组</returns>
+        public static string[] Gets(string strSessionName)
+        {
+            if (HttpContext.Current.Session[strSessionName] == null)
+            {
+                return null;
+            }
+            else
+            {
+                return (string[])HttpContext.Current.Session[strSessionName];
+            }
+        } 
+        #endregion
+
+        #region 删除某个Session对象
+        /// <summary>
+        /// 删除某个Session对象
+        /// </summary>
+        /// <param name="strSessionName">Session对象名称</param>
+        public static void Remove(string strSessionName)
+        {
+            HttpContext.Current.Session[strSessionName] = null;
+        } 
+        #endregion
+    }
+
+}
