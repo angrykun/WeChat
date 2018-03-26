4c9ddf1ba5216848378c44a7da328c27dcc980bd
angrykun <2545662859@qq.com>
2018-03-26 16:18:42 +0800

    添加configHelper

---
+++ WeChat.Solution/WeChat.Common/ConfigHelper.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using CM = System.Configuration.ConfigurationManager;
+
+namespace WeChat.Common
+{
+    /// <summary>
+    /// 配置文件Helper
+    /// </summary>
+    public class ConfigHelper
+    {
+        #region 获取webConfig中appSettings中配置项
+        /// <summary>
+        /// 获取webConfig中appSettings中配置项
+        /// </summary>
+        /// <param name="key">配置项的键</param>
+        /// <returns>配置项的值</returns>
+        public static string GetConfigValue(string key)
+        {
+            string result = string.Empty;
+            try
+            {
+                result = CM.AppSettings[key];
+            }
+            catch { }
+            return result;
+        }
+        #endregion
+
+        #region 获取webconfig当中appsettings中的配置项
+        /// <summary>
+        ///  获取webconfig当中appsettings中的配置项
+        /// </summary>
+        /// <typeparam name="T">类型</typeparam>
+        /// <param name="key">配置项的键</param>
+        /// <returns>配置项的值</returns>
+        public static T GetConfigValue<T>(string key)
+        {
+            try
+            {
+                return (T)Convert.ChangeType(CM.AppSettings[key], typeof(T));
+            }
+            catch
+            {
+                return default(T);
+            }
+        }
+        #endregion
+
+    }
+}
